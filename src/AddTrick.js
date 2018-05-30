2a585fd599c01107f0c0ab274bb47d067a8f8594
Andrew Anderson <gimlilongbow@gmail.com>
2018-05-29 23:55:29 -0400

    Pushing state up!

--- src/AddTrick.js
+++ src/AddTrick.js
@@ -1,31 +1,75 @@
 import React, { Component } from 'react';
 
 class AddTrick extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: 0,
+      title: "",
+      url: "",
+      image: "",
+      description: "",
+      done: false,
+      difficulty: 1
+    };
+  }
+
+  createTrick = (event) => {
+    console.table(this.state);
+    event.preventDefault();
+  }
+
+  updateState = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    console.log(`Name: ${name}`);
+    console.log(`Value: ${value}`);
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return(
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Add Trick</h5>
-          <div>
+          <form>
             <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Name" aria-label="Name" />
-            </div>
-
-            <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="URL" aria-label="URL" />
+              <input name="title" type="text" className="form-control" placeholder="Title" aria-label="Title" value={this.state.title} onChange={this.updateState} />
             </div>
 
             <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Screenshot URL" aria-label="Screenshot" />
+              <input name="url" type="text" className="form-control" placeholder="URL" aria-label="URL" value={this.state.url} onChange={this.updateState} />
             </div>
 
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="Description" aria-label="Screenshot" />
+            <div className="input-group mb-3">
+              <input name="image" type="text" className="form-control" placeholder="Image URL" aria-label="Image URL" value={this.state.image} onChange={this.updateState} />
             </div>
 
-          </div>
+            <div className="input-group mb-3">
+              <input name="description" type="text" className="form-control" placeholder="Description" aria-label="Description" value={this.state.description} onChange={this.updateState} />
+            </div>
+
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <label className="input-group-text" htmlFor="difficulty">Difficulty</label>
+              </div>
+              <select name="difficulty" className="custom-select" id="difficulty" value={this.state.difficulty} onChange={this.updateState}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+
+          </form>
           <p className="card-text">
-            <button className="btn btn-primary pull-center btn-block">Add</button>
+            <button onClick={this.createTrick} className="btn btn-primary pull-center btn-block">Add</button>
           </p>
         </div>
       </div>
--- src/Trick.js
+++ src/Trick.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 
 class Trick extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    Object.assign(this.state, props.trick);
-  }
-
   updateDifficulty = (event) => {
-    this.setState({ difficulty: event.target.value });
+    this.props.updateDifficulty(this.props.trick.key, event.target.value);
   }
 
-  updateCompletionStatus = () => {
-    this.setState({ done: !this.state.done })
+  updateCompletionStatus = (event) => {
+    this.props.updateCompletion(this.props.trick.key);
   }
 
   render() {
     return (
       <div className="card">
-        <a target="_blank" href={this.state.url}>
-          <img className="card-img-top" src={this.state.image} alt={this.state.title} />
+        <a target="_blank" href={this.props.trick.url}>
+          <img className="card-img-top" src={this.props.trick.image} alt={this.props.trick.title} />
         </a>
         <div className="card-body">
-          <h3 className="card-title">{this.state.title}</h3>
+          <h3 className="card-title">{this.props.trick.title}</h3>
           <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Difficulty and Success">
             <div className="input-group">
               <div className="input-group-prepend">
                 <label className="input-group-text" htmlFor="difficulty">Difficulty</label>
               </div>
-              <select className="custom-select" id="difficulty" value={this.state.difficulty} onChange={this.updateDifficulty}>
+              <select className="custom-select" id="difficulty" value={this.props.trick.difficulty} onChange={this.updateDifficulty}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -38,13 +32,13 @@ class Trick extends Component {
             </div>
 
             <div className="btn-group">
-              <button type="button" onClick={this.updateCompletionStatus} className={this.state.done === false ? "btn btn-info" : "btn btn-success"}>&#10004;</button>
+              <button type="button" onClick={this.updateCompletionStatus} className={this.props.trick.done === false ? "btn btn-info" : "btn btn-success"}>&#10004;</button>
             </div>
           </div>
 
         </div>
         <div className="card-body">
-          <p className="card-text">{this.state.description}</p>
+          <p className="card-text">{this.props.trick.description}</p>
         </div>
       </div>
     );
