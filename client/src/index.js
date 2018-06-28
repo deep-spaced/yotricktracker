ebe80bcbe95d19ff444b0da692ec8ab563f611fb
Andrew Anderson <gimlilongbow@gmail.com>
2018-06-28 03:10:09 -0400

    Adding in Rails!

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// Potential conflict with Rails.
+// import registerServiceWorker from './registerServiceWorker';
+// registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
